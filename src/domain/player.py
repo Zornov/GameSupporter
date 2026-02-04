56984965b6ed8bdd8c9b47c03a1616c3441910b5
Zorinov <zorov.me@gmail.com>
2026-02-04 22:35:36 +0200

    Delete src directory

--- src/domain/player.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from typing import Optional
-from .body_parts import Head, Body
-
-@dataclass
-class Player:
-    isEnemy: bool = True
-    head: Optional[Head] = None
-    body: Optional[Body] = None
